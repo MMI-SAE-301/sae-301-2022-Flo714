750464b076f2797490c493bb5962da521e0b6b9c
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-24 16:48:11 +0200

    INSERT création table Montre

--- bdd.sql
+++ bdd.sql
@@ -8,7 +8,18 @@ CREATE TABLE Materiaux (
     PRIMARY KEY (ID_Materiaux)
     )
 
-
+CREATE TABLE Montre (
+    ID_Montre uuid not null default uuid_generate_v4(), 
+    Bracelet_Montre VARCHAR, 
+    Ecran_Montre VARCHAR, 
+    Boutons_Montre VARCHAR, 
+    Boitier_Montre VARCHAR, 
+    id_materiaux uuid,
+    commander boolean not null default false,
+    id_utilisateurs uuid references auth.users not null default uid(), 
+    FOREIGN KEY (id_materiaux) references materiaux(id_materiaux), 
+    PRIMARY KEY (ID_Montre)
+    )
 
 
 
