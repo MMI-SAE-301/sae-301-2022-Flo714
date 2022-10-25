75153321ee5f292e582cb851a109f7968947d44a
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-25 10:52:40 +0200

    formulaire data

---
+++ src/pages/montre/exemple/[data.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import FormSvg from "../../../components/Formmontre.vue";
+defineProps<{
+  data: string;
+}>();
+</script>
+<template>
+  <FormSvg :data="JSON.parse(data)" />
+</template>
