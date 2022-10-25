5b4fbfe67f41fd8ee3a75fd41d25a6aa970d6e3e
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-25 15:40:55 +0200

    Début header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,22 @@
-<script type lang="ts">
+<script setup lang="ts">
+import Logo from "./Logo.vue";
 
 </script>
 
 <template>
-    <main class="">
-        <h1>Bonjour</h1>
-    </main>
-</template>
+    <header class=" px-5">
+        <a href="#content" class="sr-only focus:not-sr-only text-lg text-blanc"> Passez au contenu </a>
+        <nav class="flex">
+          <div class="flex justify-between items-center">
+                <RouterLink to="/">
+                    <Logo class="w-36" />
+                </RouterLink>
+          </div>
+          <ul class="flex  gap-6 text-xl mx-6 py-4" >
+            <li class="my-3">
+              <RouterLink class="my-6" to="/montres/new">Personnalisation</RouterLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </template>
