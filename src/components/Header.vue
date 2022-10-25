73226e524e81cd655a1fe83a83b5390b6847fa34
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-25 15:57:13 +0200

    import des couleurs + test

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,25 +4,25 @@ import Logo from "./Logo.vue";
 </script>
 
 <template>
-    <header class=" px-5">
+    <header class="bg-noir px-5">
         <a href="#content" class="sr-only focus:not-sr-only text-lg text-blanc"> Passez au contenu </a>
         <nav class="flex justify-between">
           <div class="flex items-center">
                 <RouterLink to="/">
-                    <Logo class="w-36" />
+                    <Logo class="w-16 h-16 my-3" />
                 </RouterLink>
           </div>
-          <ul class="flex items-center gap-24 text-xl mx-6 py-4" >
-            <li class="my-3">
+          <ul class="flex items-center gap-24 text-lg mx-6" >
+            <li class="my-3 text-blanc">
               <RouterLink class="my-6" to="/montre">Nos montres</RouterLink>
             </li>
-            <li class="my-3">
+            <li class="my-3 text-blanc">
               <RouterLink class="my-6" to="/montre/perso">Mes modèles</RouterLink>
             </li>
-            <li class="my-3">
+            <li class="my-3 text-blanc">
               <RouterLink class="my-6" to="/montre/new">Personnalisation</RouterLink>
             </li>
-            <li class="my-3">
+            <li class="my-3 text-blanc">
               <RouterLink class="my-6" to="/LoginLogoutView">Se connecter</RouterLink>
             </li>
           </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ const formKitTailwind = require("@formkit/themes/tailwindcss");
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      'blanc':'#ffffff',
+      'noir':'#000000'
+    },
+    extend: {}
   },
   plugins: [
     require("@tailwindcss/typography"),
