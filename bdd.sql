889efc6b2a3644674dadaa77b49589f32b9d0c0e
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-25 08:26:59 +0200

    Création police "Les gens peuvent modifier"

--- bdd.sql
+++ bdd.sql
@@ -48,3 +48,11 @@ ON public.montre
 FOR INSERT 
 TO authenticated 
 WITH CHECK (true);
+
+CREATE POLICY "Les gens peuvent modifier"
+ON public.montre
+FOR UPDATE USING(
+    (uid() = id_utilisateurs) AND (commander = false)
+) WITH CHECK (
+    uid() IN ( SELECT montre_1.id_utilisateurs FROM montre montre_1)
+)
