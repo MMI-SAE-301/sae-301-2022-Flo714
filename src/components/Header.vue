b9858093b3ed79c7a7b972251d7c1e612677fcb1
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-25 16:12:04 +0200

    Hover blanc sur les menu du header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,17 +12,17 @@ import Logo from "./Logo.vue";
                     <Logo class="w-16 h-16 my-3" />
                 </RouterLink>
           </div>
-          <ul class="flex items-center gap-20 text-xl mx-6 text-blanc font-inter font-bold" >
+          <ul class="flex items-center text-xl mx-4 text-blanc font-inter font-bold" >
             <li class="my-3">
-              <RouterLink class="my-6" to="/montre">Nos montres</RouterLink>
+              <RouterLink class="my-6 p-12 hover:bg-blanc hover:text-noir" to="/montre">Nos montres</RouterLink>
             </li>
             <li class="my-3">
-              <RouterLink class="my-6" to="/montre/perso">Mes modèles</RouterLink>
+              <RouterLink class="my-6 p-12 hover:bg-blanc hover:text-noir" to="/montre/perso">Mes modèles</RouterLink>
             </li>
             <li class="my-3">
-              <RouterLink class="my-6" to="/montre/new">Personnalisation</RouterLink>
+              <RouterLink class="my-6 p-12 hover:bg-blanc hover:text-noir" to="/montre/new">Personnalisation</RouterLink>
             </li>
-            <li class="my-3  bg-rouge py-2 px-8 rounded-3xl hover:bg-blanc hover:text-noir">
+            <li class="my-3 ml-4 bg-rouge py-2 px-8 rounded-3xl hover:bg-blanc hover:text-noir">
               <RouterLink class="my-6" to="/LoginLogoutView">Se connecter</RouterLink>
             </li>
           </ul>
