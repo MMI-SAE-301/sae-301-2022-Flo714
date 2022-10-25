8c0de7ebf8bb92f7fb285b9572815c8887860dcf
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-25 16:35:30 +0200

    modif couleur trait + my

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,22 +8,18 @@
 
       <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
 
-        <div class="text-xs uppercase text-white font-bold mb-6 border-b-2 border-rouge pb-4 w-1/4">
-          Lien
-        </div>
+        <div class="text-xs uppercase text-white font-bold mb-6 border-b-2 border-blanc pb-4 w-1/4">Lien</div>
         <RouterLink class="hover:text-gray-500 font-semibold duration-500" to="/">Accueil</RouterLink>
-        <RouterLink class="text-gris my-3 block  font-semibold duration-700" to="/montre/new">Personnalisation</RouterLink>
-        <RouterLink class="text-gris my-3 block  font-semibold duration-700" to="/montre">Nos montres</RouterLink>
-        <RouterLink class="text-gris my-3 block  font-semibold duration-400" to="/montre/perso">Mes modèles</RouterLink>
-        <RouterLink class="text-gris my-3 duration-500 bg-rouge py-2 px-8 rounded-3xl hover:bg-blanc hover:text-noir" 
+        <RouterLink class="text-gris my-6 block  font-semibold duration-700" to="/montre/new">Personnalisation</RouterLink>
+        <RouterLink class="text-gris my-6 block  font-semibold duration-700" to="/montre">Nos montres</RouterLink>
+        <RouterLink class="text-gris my-6 block  font-semibold duration-400" to="/montre/perso">Mes modèles</RouterLink>
+        <RouterLink class="text-gris my-6 duration-500 bg-rouge py-2 px-8 rounded-3xl hover:bg-blanc hover:text-noir" 
             to="/montre/perso">Se connecter</RouterLink>
       </div>
 
       <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12 flex flex-col ">
 
-        <div class="text-xs uppercase text-white font-bold mb-6 border-b-2 border-rouge pb-4 w-1/2">
-          Obtenir de l'aide
-        </div>
+        <div class="text-xs uppercase text-white font-bold mb-6 border-b-2 border-blanc pb-4 w-1/2">Obtenir de l'aide</div>
         <a class="text-gris my-3  hover:text-gray-100 text-xs sm:text-lg  font-semibold duration-700 "
           href="florentin.demortiere@edu.univ-fcomte.fr">florentin.demortiere@edu.univ-fcomte.fr</a>
       </div>
