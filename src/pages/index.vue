fa712a5c56e51f172ddddbac09978f9c135f8cc5
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-26 08:34:37 +0200

    liste montre dans template

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,20 @@
 <template>
   <section class="p-0 m-0 w-full">
     <Carrousel />
+    <div>
+        <div class="flex flex-wrap gap-2">
+      <div class="w-64" v-for="montre in exemples">
+        <RouterLink
+          :to="{
+            name: 'montre-exemple-data',
+            params: { data: JSON.stringify(montre) },
+          }"
+        >
+          <MontreSvg class="w-64" v-bind="montre" />
+        </RouterLink>
+      </div>
+    </div>
+    </div>
   </section>
 </template>
 
