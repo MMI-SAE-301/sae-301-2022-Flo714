7f3602cea0863a62d583061b244002b8a05ed06f
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-26 17:24:14 +0200

    essaie design connection

--- src/components/LoginLogout.vue
+++ src/components/LoginLogout.vue
@@ -26,31 +26,29 @@
         </div>
     </div>
     <div class="bg-blanc font-inter font-bold text-noir">
-        <div class="container mx-auto p-8">
-            <div class="max-w-lg w-full mx-auto">
-                <div class="bg-white rounded-lg shadow-2xl">
-                    <div class="p-8 flex justify-center">
-                        <button v-if="user" @pointerdown="supabase.auth.signOut()">
-                         Se déconnecter ({{ user.email }})
-                        </button>
+        <div class="w-full">
+            <div class="border-2 border-noir border-opacity-40">
+                <div class="p-8 flex justify-center">
+                    <button v-if="user" @pointerdown="supabase.auth.signOut()">
+                     Se déconnecter ({{ user.email }})
+                    </button>
+                    <FormKit
+                         v-else
+                         type="form"
+                         :submit-label="nvlUtilisateur ? 'S\'inscrire' : 'Se connecter'"
+                         @submit="signIn"
+                         :submit-attrs="{ classes: { input: 'bg-rouge text-blanc px-4 py-2 rounded-lg hover:bg-noir' } }"
+                         >
+                        <FormKit name="email" label="Votre eMail" type="email" label-class="my-6 text-3xl font-bold" class="w-full p-3 rounded bg-blanc border focus:outline-none"/>
+                        <FormKit name="password" label="Mot de passe" type="password" label-class="my-6 text-3xl font-bold" class=" my-10 w-full p-3 rounded bg-blanc border  focus:outline-none"/>
                         <FormKit
-                             v-else
-                             type="form"
-                             :submit-label="nvlUtilisateur ? 'S\'inscrire' : 'Se connecter'"
-                             @submit="signIn"
-                             :submit-attrs="{ classes: { input: 'bg-rouge text-blanc px-4 py-2 rounded-lg hover:bg-noir' } }"
-                             >
-                            <FormKit name="email" label="Votre eMail" type="email" label-class="block mt-4  mb-2 text-xl font-bold" class="block w-full p-3 rounded bg-blanc border focus:outline-none"/>
-                            <FormKit name="password" label="Mot de passe" type="password" label-class="block mt-4 mb-2 text-xl font-bold" class="block w-full p-3 rounded bg-blanc border  focus:outline-none"/>
-                            <FormKit
-                            label="S'inscrire"
-                            name="nvlUtilisateur"
-                            type="checkbox"
-                            v-model="nvlUtilisateur"
-                            class=" p-3 mt-4 text-blanc flex jus"
-                            />
-                        </FormKit>
-                    </div>
+                        label="S'inscrire"
+                        name="nvlUtilisateur"
+                        type="checkbox"
+                        v-model="nvlUtilisateur"
+                        class=" p-3 mt-4 text-blanc flex jus"
+                        />
+                    </FormKit>
                 </div>
             </div>
         </div>
