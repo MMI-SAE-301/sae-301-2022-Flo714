92d8e620de49c96976741614af76a61131172101
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-26 19:59:05 +0200

    Adaptation boutons G F

--- src/components/LoginLogout.vue
+++ src/components/LoginLogout.vue
@@ -15,20 +15,6 @@ import LoginFacebook from "./LoginFacebook.vue";
  const nvlUtilisateur = ref(false);
  </script>
 <template>
-    <div class="flex justify-center gap-24">
-        <div>
-            <button class="flex items-center gap-6 bg-bleu px-6 py-4 rounded-lg font-inter font-bold text-xl hover:bg-noir hover:text-blanc duration-300" @pointerdown="supabase.auth.signIn({provider: 'facebook'})"><LoginFacebook />
-            Se connecter avec Facebook
-            </button>
-
-        </div>
-        <div>
-            <button class="flex items-center gap-6 bg-rouge px-6 py-4 rounded-lg font-inter font-bold text-xl hover:bg-noir hover:text-blanc duration-300" @pointerdown="supabase.auth.signIn({provider: 'google'})"><LoginGoogle />
-            Se connecter avec Google
-            </button>
-            
-        </div>
-    </div>
     <div class="bg-blanc font-inter font-bold text-noir">
         <div class="w-full">
             <div class="border-2 border-noir border-opacity-40">
@@ -55,6 +41,20 @@ import LoginFacebook from "./LoginFacebook.vue";
                     </FormKit>
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="flex justify-center gap-24 my-20">
+        <div>
+            <button class="flex items-center gap-6 bg-bleu px-6 py-4 rounded-lg font-inter font-bold text-xl hover:bg-noir hover:text-blanc duration-300" @pointerdown="supabase.auth.signIn({provider: 'facebook'})"><LoginFacebook />
+            Se connecter avec Facebook
+            </button>
+
+        </div>
+        <div>
+            <button class="flex items-center gap-6 bg-rouge px-6 py-4 rounded-lg font-inter font-bold text-xl hover:bg-noir hover:text-blanc duration-300" @pointerdown="supabase.auth.signIn({provider: 'google'})"><LoginGoogle />
+            Se connecter avec Google
+            </button>
+            
         </div>
     </div>
 </template>
