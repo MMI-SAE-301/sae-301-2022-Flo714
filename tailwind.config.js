e364b7a582e87d85bed7e0a7ff693ccd3771e4a4
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-26 10:20:36 +0200

    import box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
     extend: {
       "fontFamily": {
         "inter": "Inter"
+      },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
       }
     }
   },
