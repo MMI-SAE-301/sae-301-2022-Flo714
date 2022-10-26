290a751febe3efc4ebaf475388705a7331f18eab
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-26 11:26:55 +0200

    Article 03

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -22,7 +22,7 @@
     </div>
     <div class="mx-10 mt-24 mb-10">
         <div class="bg-bleu text-blanc ml-10 mr-96 pl-10 rounded-xl pt-10 pb-24">
-            <h2 class="text-3xl pt-10 pb-24">Pratiquer du sport avec TiK•TaK</h2>
+            <h2 class="text-4xl pt-10 pb-24 font-bold">Pratiquer du sport avec TiK•TaK</h2>
             <div class="py-10">
                 <p class="text-2xl pr-[50%] py-5 pl-5">Pratiquer du sport vous permettra d’être en meilleure santé et d’être plus heureux.  Alors pourquoi ne pas essayer la TiK•TaK durant vos meilleures activités physiques.  Elle dispose de multiples fonctionnalités pour vous satisfaire. Vous pouvez ainsi pratiquer votre passion.</p>
                 <p class="text-2xl pr-[50%] py-5 pl-5">Personnaliser votre montre à votre goût pour la rendre unique.  </p>
@@ -37,7 +37,7 @@
             <img class="w-[50%] rounded-xl" src="../assets/Caoutchouc.jpg" alt="Caoutchouc">
         </div>
         <div class="bg-bleu text-blanc mr-10 ml-96 pr-10 rounded-xl pt-10 pb-24">
-            <h2 class="text-3xl pt-10 pb-24 ml-[35vw]">Des matériaux d’exception</h2>
+            <h2 class="text-4xl pt-10 pb-24 ml-[35vw] font-bold">Des matériaux d’exception</h2>
             <div class="py-10">
                 <p class="text-2xl pl-[50%] pr-5">TiK•TaK utilise des matériaux d’exception comme du cuir, du carbone et du Caoutchouc.</p>
                 <p class="text-2xl pl-[50%] pr-5">Le cuir provient de peau de vache naturelle fabriquée sur mesure.</p>
@@ -45,6 +45,18 @@
                 <p class="text-2xl pl-[50%] pr-5">Le caoutchouc est fabriquées à partir de latex qui est la résine d'un arbre, l'Hévéa.</p>
                 <p class="text-2xl pl-[50%] pr-5">Ces matériaux composeront vos bracelets de montre à votre guise.</p>
             </div>
+        </div>
+    </div>
+    <div class="mx-10 mt-24 mb-10">
+        <div class="bg-bleu text-blanc ml-10 mr-96 pl-10 rounded-xl pt-10 pb-24">
+            <h2 class="text-4xl pt-10 pb-24 font-bold">TiK•TaK, une fabrication Française</h2>
+            <div class="py-10">
+                <p class="text-2xl pr-[50%] py-5 pl-5">TiK•TaK est une marque 100 % Française. L’usine d’assemblage se situe à Germagny, dans le département de Saône et Loire. Tous nos produits sont assemblés ici pour permettre une plus grande facilité de livraison de la marchandise.</p>
+                <p class="text-2xl pr-[50%] py-5 pl-5">De plus, nous livrons seulement la France et les frontaliers. Vous pourrez ainsi montrer lors d’entraînement ou de compétitions internationale que TiK•TaK est la meilleur montre du marché</p>
+            </div>
+        </div>
+        <div class="flex justify-end -mt-[50vh] mr-40">
+            <img class="w-[50%] rounded-xl" src="../assets/Tik Tak_fr.jpg" alt="Pratiquer du Sport">
         </div>
     </div>
 </template>
