d0ea8287f79ffb34f04543c9c9df49c96966b431
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-26 14:22:25 +0200

    css bouton radio materiaux

--- src/components/Choixmat.vue
+++ src/components/Choixmat.vue
@@ -27,8 +27,8 @@ const selected = ref(plans[0])
       <div class="mx-auto w-full max-w-md">
         <RadioGroup v-model="selected">
             <RadioGroupLabel class="sr-only">Server size</RadioGroupLabel>
-                <div class="space-y-2">
-                    <RadioGroupOption
+                <div class="flex gap-10">
+                    <RadioGroupOption class="text-blanc"
                         as="template"
                         v-for="plan in plans"
                         :key="plan.materiaux"
@@ -38,26 +38,16 @@ const selected = ref(plans[0])
                           active
                             ? 'ring-2 ring-noir'
                             : '',
-                          checked ? 'bg-noir bg-opacity-5 text-noir ' : 'bg-blanc ',
+                          checked ? 'bg-noir text-blanc ' : ' bg-rouge',
                             ]"
-                            class="cursor-pointer rounded-lg px-5 py-4 border-2">
-                            <div class="flex w-full justify-between">
-                                <div class="flex items-center">
-                                    <div class="text-sm">
-                                        <RadioGroupLabel
-                                            as="p"
-                                            :class="checked ? 'text-noir' : 'text-gray-900'"
-                                            class="font-medium">
-                                            {{ plan.materiaux }}
-                                        </RadioGroupLabel>
-                                    </div>
-                                </div>
-                                <div v-show="checked" class="shrink-0 text-white">
-                                    <svg class="h-6 w-6" viewBox="0 0 24 24" fill="none">
-                                        <circle cx="12" cy="12" r="12" fill="#000" fill-opacity="0.2" />
-                                        <path d="M7 13l3 3 7-7" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg>
-                                </div>
+                            class="cursor-pointer rounded-lg px-10 py-4 border-2 ">
+                            <div class="text-sm">
+                                <RadioGroupLabel
+                                    as="p"
+                                    :class="checked ? 'text-blanc' : 'text-blanc'"
+                                    class="font-medium ">
+                                    {{ plan.materiaux }}
+                                </RadioGroupLabel>
                             </div>
                         </div>
                     </RadioGroupOption>
