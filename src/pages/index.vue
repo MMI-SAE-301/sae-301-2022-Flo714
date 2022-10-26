49f666c52551e8639f132a3d726a9464cd602d5f
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-26 10:01:01 +0200

    Mise en page Carrousel

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,12 @@
 <template>
-    <section>
-        
+    <section class="p-0 m-0">
+        <div >
+            <Carrousel />
+        </div>
     </section>
   </template>
   
   <script setup lang="ts">
+    import Carrousel from '../components/Carrousel.vue';
+
   </script>
