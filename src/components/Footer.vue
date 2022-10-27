032186fe4341637f8d6945f50ddf39f402ab56e6
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-27 11:00:02 +0200

    Dark footer bouton se connecter

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -15,7 +15,7 @@
                 <RouterLink class=" my-6 block  font-semibold duration-500 hover:text-rouge" to="/montre/new">Personnalisation</RouterLink>
                 <RouterLink class=" my-6 block  font-semibold duration-500 hover:text-rouge" to="/montre">Nos montres</RouterLink>
                 <RouterLink class=" my-6 block  font-semibold duration-500 hover:text-rouge" to="/montre/perso">Mes modèles</RouterLink>
-                <RouterLink class=" my-6 duration-500 bg-rouge py-2 px-8 rounded-3xl hover:bg-blanc hover:text-noir" 
+                <RouterLink class=" my-6 duration-500 bg-rouge py-2 px-8 rounded-3xl text-blanc hover:bg-blanc hover:text-noir dark:hover:bg-noir dark:hover:text-blanc" 
                     to="/montre/perso">Se connecter</RouterLink>
             </div>
             <div class="p-5 w-10/12 sm:w-4/12 md:w-3/12 flex flex-col ">
