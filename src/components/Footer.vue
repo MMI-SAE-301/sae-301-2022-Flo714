73241ec9887b274c626cfdf38528642edcc621c6
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-27 10:22:50 +0200

    Changement couleur footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,18 +8,18 @@
 
             <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
 
-                <div class="text-xs uppercase text-white font-bold mb-6 border-b-2 border-blanc pb-4 w-1/4">
+                <div class="uppercase text-white text-sm font-bold mb-6 border-b-2 border-blanc pb-4 w-1/4">
                     Lien
                 </div>
-                <RouterLink class="text-gris font-semibold duration-500 hover:text-rouge" to="/">Accueil</RouterLink>
-                <RouterLink class="text-gris my-6 block  font-semibold duration-500 hover:text-rouge" to="/montre/new">Personnalisation</RouterLink>
-                <RouterLink class="text-gris my-6 block  font-semibold duration-500 hover:text-rouge" to="/montre">Nos montres</RouterLink>
-                <RouterLink class="text-gris my-6 block  font-semibold duration-500 hover:text-rouge" to="/montre/perso">Mes modèles</RouterLink>
-                <RouterLink class="text-gris my-6 duration-500 bg-rouge py-2 px-8 rounded-3xl hover:bg-blanc hover:text-noir" 
+                <RouterLink class=" font-semibold duration-500 hover:text-rouge" to="/">Accueil</RouterLink>
+                <RouterLink class=" my-6 block  font-semibold duration-500 hover:text-rouge" to="/montre/new">Personnalisation</RouterLink>
+                <RouterLink class=" my-6 block  font-semibold duration-500 hover:text-rouge" to="/montre">Nos montres</RouterLink>
+                <RouterLink class=" my-6 block  font-semibold duration-500 hover:text-rouge" to="/montre/perso">Mes modèles</RouterLink>
+                <RouterLink class=" my-6 duration-500 bg-rouge py-2 px-8 rounded-3xl hover:bg-blanc hover:text-noir" 
                     to="/montre/perso">Se connecter</RouterLink>
             </div>
             <div class="p-5 w-10/12 sm:w-4/12 md:w-3/12 flex flex-col ">
-                <div class="text-xs uppercase text-white font-bold mb-6 border-b-2 border-blanc pb-4 w-1/2">
+                <div class="uppercase text-white font-bold mb-6 border-b-2 border-blanc pb-4 w-1/2 text-sm">
                     Obtenir de l'aide
                 </div>
                 <a class="my-3  hover:text-rouge text-xs sm:text-lg  font-semibold duration-700 " href="florentin.demortiere@edu.univ-fcomte.fr">florentin.demortiere@edu.univ-fcomte.fr</a>
