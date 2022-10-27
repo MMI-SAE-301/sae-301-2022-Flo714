0f17e75f4e2477caf03c62127b0b49749d2c6ef9
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-27 11:11:04 +0200

    Dark loginlogout texte

--- src/components/LoginEmail.vue
+++ src/components/LoginEmail.vue
@@ -12,10 +12,11 @@
                  :submit-attrs="{ classes: { input: 'bg-rouge text-blanc text-3xl px-6 py-4 rounded-lg hover:bg-noir duration-500 hover:dark:text-noir hover:dark:bg-blanc' } }"
 
                  >
-                <div class="my-6"><FormKit name="email" label="Votre Email" type="email" input-class="w-80 bg-blanc focus:outline-none" label-class=" my-6 text-3xl font-bold"/></div>
-                <div class="my-6"><FormKit name="password" label="Mot de passe" type="password" input-class="w-80 bg-blanc focus:outline-none" label-class="my-20 text-3xl font-bold"/></div>
+                <div class="my-6"><FormKit name="email" label="Votre Email" type="email" input-class="w-80 bg-blanc focus:outline-none" label-class=" my-6 text-3xl font-bold dark:text-blanc"/></div>
+                <div class="my-6"><FormKit name="password" label="Mot de passe" type="password" input-class="w-80 bg-blanc focus:outline-none " label-class="my-20 text-3xl font-bold dark:text-blanc"/></div>
                 <FormKit
                     label="S'inscrire"
+                    label-class="dark:text-blanc"
                     name="nvlUtilisateur"
                     type="checkbox"
                     v-model="nvlUtilisateur"/>
