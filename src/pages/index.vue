9d04ccc50077635f823c4e96b1d7c94cf5c3d8fc
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-10-27 08:23:24 +0200

    Changement taille général article 1 et 2

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -20,30 +20,30 @@
                      ma montre</p></RouterLink>
         </div>
     </div>
-    <div class="mx-10 mt-24 mb-10">
-        <div class="bg-bleu text-blanc ml-10 mr-96 pl-10 rounded-xl pt-10 pb-24">
-            <h2 class="text-4xl pt-10 pb-24 font-bold">Pratiquer du sport avec TiK•TaK</h2>
+    <div class="mx-10">
+        <div class="bg-bleu text-blanc ml-10 mr-96 pl-10 rounded-xl pt-10">
+            <h2 class="text-3xl pt-10 font-bold">Pratiquer du sport avec TiK•TaK</h2>
             <div class="py-10">
-                <p class="text-2xl pr-[40%] py-5 pl-5">Pratiquer du sport vous permettra d’être en meilleure santé et d’être plus heureux.  Alors pourquoi ne pas essayer la TiK•TaK durant vos meilleures activités physiques.  Elle dispose de multiples fonctionnalités pour vous satisfaire. Vous pouvez ainsi pratiquer votre passion.</p>
-                <p class="text-2xl pr-[40%] py-5 pl-5">Personnaliser votre montre à votre goût pour la rendre unique.  </p>
+                <p class="text-xl pr-[40%] py-5 pl-5">Pratiquer du sport vous permettra d’être en meilleure santé et d’être plus heureux.  Alors pourquoi ne pas essayer la TiK•TaK durant vos meilleures activités physiques.  Elle dispose de multiples fonctionnalités pour vous satisfaire. Vous pouvez ainsi pratiquer votre passion.</p>
+                <p class="text-xl pr-[40%] py-5 pl-5">Personnaliser votre montre à votre goût pour la rendre unique.  </p>
             </div>
         </div>
         <div class="flex justify-end -mt-[55vh] mr-6">
-            <img class="w-[50%] rounded-xl" src="../assets/Sport.jpg" alt="Pratiquer du Sport">
+            <img class="w-[45%] rounded-xl" src="../assets/Sport.jpg" alt="Pratiquer du Sport">
         </div>
     </div>
     <div class="mx-10 my-72">
-        <div class="flex justify-start -mb-[80vh] ml-10">
-            <img class="w-[50%] rounded-xl" src="../assets/Caoutchouc.jpg" alt="Caoutchouc">
+        <div class="flex justify-start -mb-[70vh]">
+            <img class="w-[45%] rounded-xl" src="../assets/Caoutchouc.jpg" alt="Caoutchouc">
         </div>
-        <div class="bg-bleu text-blanc mr-10 ml-96 pr-10 rounded-xl pt-10 pb-24">
-            <h2 class="text-4xl pt-10 pb-24 ml-[35vw] font-bold">Des matériaux d’exception</h2>
+        <div class="bg-bleu text-blanc mr-10 ml-96 pr-10 rounded-xl pt-10 pb-12">
+            <h2 class="text-3xl pt-10 pb-10 ml-[20vw] font-bold">Des matériaux d’exception</h2>
             <div class="">
-                <p class="text-2xl pl-[50%] pr-5">TiK•TaK utilise des matériaux d’exception comme du cuir, du carbone et du Caoutchouc.</p>
-                <p class="text-2xl pl-[50%] pr-5">Le cuir provient de peau de vache naturelle fabriquée sur mesure.</p>
-                <p class="text-2xl pl-[50%] pr-5">Les montres en carbone sont fabriqué par nos imprimantes 3D. Chaque modèle sera donc fait sur mesure par nos techniciens.</p>
-                <p class="text-2xl pl-[50%] pr-5">Le caoutchouc est fabriquées à partir de latex qui est la résine d'un arbre, l'Hévéa.</p>
-                <p class="text-2xl pl-[50%] pr-5">Ces matériaux composeront vos bracelets de montre à votre guise.</p>
+                <p class="text-xl pl-[35%] pr-5">TiK•TaK utilise des matériaux d’exception comme du cuir, du carbone et du Caoutchouc.</p>
+                <p class="text-xl pl-[35%] pr-5">Le cuir provient de peau de vache naturelle fabriquée sur mesure.</p>
+                <p class="text-xl pl-[35%] pr-5">Les montres en carbone sont fabriqué par nos imprimantes 3D. Chaque modèle sera donc fait sur mesure par nos techniciens.</p>
+                <p class="text-xl pl-[35%] pr-5">Le caoutchouc est fabriquées à partir de latex qui est la résine d'un arbre, l'Hévéa.</p>
+                <p class="text-xl pl-[35%] pr-5">Ces matériaux composeront vos bracelets de montre à votre guise.</p>
             </div>
         </div>
     </div>
